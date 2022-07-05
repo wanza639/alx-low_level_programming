a8bf7c430b8001a67ebfba1fed3be0882bbd5cac
Elizabeth Wanza <ewanza99@gmail.com>
2022-07-04 18:07:08 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,12 @@
 
 int main(void)
 {
-  int c;
-  
+int c;
 for (c = 97; c < 123; c++)
-    {
-      if (c != 101 && c != 113)
-	putchar(c);
-    }
-  putchar('\n');
+{
+if (c != 101 && c != 113)
+putchar(c);
+}
+putchar('\n');
 return (0);
 }
