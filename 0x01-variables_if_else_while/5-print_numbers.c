4d121ed81f45fe94fefad8e7a385eb4cd65323bd
Elizabeth Wanza <ewanza99@gmail.com>
2022-07-04 18:08:58 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,13 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
+
 int main(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-      printf("%i", i);
+printf("%i", i);
 }
 printf("\n");
 return (0);
