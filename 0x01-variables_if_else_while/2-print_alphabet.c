6079438e1a879bb28e3919420f1100b6af300ad3
Elizabeth Wanza <ewanza99@gmail.com>
2022-07-04 17:56:59 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,13 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
+
 int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-      putchar(ch);
+putchar(ch);
 }
 putchar('\n');
 return (0);
