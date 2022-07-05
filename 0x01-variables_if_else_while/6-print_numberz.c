9c68d16a34ce94c6645d5553bd2659de4d564d64
Elizabeth Wanza <ewanza99@gmail.com>
2022-07-04 18:10:44 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,13 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
+
 int main(void)
 {
 char c;
 for (c = '0'; c <= '9'; c++)
 {
-      putchar(c);
+putchar(c);
 }
 putchar('\n');
 return (0);
